3d97471b188bcda801cfad553c41797134539802
Abhishek <abhee1443901550@gmail.com>
2026-09-08 21:08:27 +0530

    Add LC977 Squares Of A Sorted Array

---
+++ src/Arrays/LC977_SquaresOfASortedArray.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+public class LC977_SquaresOfASortedArray
+{
+    public static int[] sortedSquares(int[] nums)
+    {
+        int[] newarr = new int[nums.length];
+        int sI = 0;
+        int eI = nums.length-1;
+        int pI = nums.length-1;
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            nums[i] = nums[i]*nums[i];
+        }
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            if(nums[sI] < nums[eI])
+            {
+                newarr[pI] = nums[eI] ;
+                eI--;
+            }
+
+            else if(nums[sI] >= nums[eI])
+            {
+                newarr[pI] = nums[sI] ;
+                sI++;
+            }
+            else if(eI == sI)
+            {
+                newarr[pI] = nums[sI];
+            }
+            pI--;
+        }
+        return newarr ;
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {-7,-3,2,3,11};
+        arr = sortedSquares(arr);
+        for(int value : arr)
+            System.out.print(value+" ");
+    }
+}
