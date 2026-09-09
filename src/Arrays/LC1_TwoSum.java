dfe79ac354ae4a444ea054e40e5b3e1978f07ff8
Abhishek Sharma <abhee1443901550@gmail.com>
2026-09-09 18:01:34 +0530

    Update LC1_TwoSum.java

--- src/Arrays/LC1_TwoSum.java
+++ src/Arrays/LC1_TwoSum.java
@@ -6,7 +6,7 @@ public class LC1_TwoSum
         int n = nums.length;
         int[] Out = new int[2];
         for(int i = 0;i < n-1;i++){
-            for(int y = i+1;y<n;y++){
+            for(int y = i+1;y<n;y++)
                 if(nums[i]+nums[y]==target) {
                     Out[0] = i;
                     Out[1] = y;
@@ -21,10 +21,11 @@ public class LC1_TwoSum
         int[] arr = {2,7,11,15};
         int target = 9;
         int[] newArr = twoSum(arr,target);
-        System.out.print("The indexs whose sum equals to target value is : ");
+        System.out.print("Indices of the two numbers : [");
         for(int value : newArr)
         {
-            System.out.print(value+" ");
+            System.out.print(value+",");
         }
+        System.out.print("]");
     }
 }
