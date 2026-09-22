7afaf510f292534a50a9beb3b835af03affa9165
Abhishek Sharma <abhee1443901550@gmail.com>
2026-09-22 19:23:29 +0530

    Add LC1672_RichestCustomerWealth class implementation

---
+++ src/Arrays/1672_RichestCustomerWealth.java
@@ -0,0 +1,18 @@
+class LC1672_RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int sum , max = 0;
+        for(int i = 0 ; i < accounts.length ; i++)
+        {
+            sum = 0;
+            for(int j = 0 ; j < accounts[i].length ; j++)
+            {
+                sum +=accounts[i][j];
+            }
+            if(sum > max )
+            {
+                max = sum ;
+            }
+        }
+        return max ;
+    }
+}
